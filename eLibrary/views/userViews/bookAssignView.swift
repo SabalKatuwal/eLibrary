615d591dcacf25e1775aa7c626422d6ac8a9cfa9
SabalKatuwal <sabal.katuwal1@gmail.com>
2022-10-21 21:36:56 +0545

    scan button on assignUserView

--- eLibrary/views/userViews/bookAssignView.swift
+++ eLibrary/views/userViews/bookAssignView.swift
@@ -12,46 +12,91 @@ import SwiftUI
 import AuthenticationServices
 
 struct bookAssignView: View {
+    @State private var isActive = false
     var body: some View {
-//        VStack{
-//            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-//
-//            Spacer()
-//
-//            Button {
-//                //scan garne ho
-//            } label: {
-//                Image(systemName: "qrcode.viewfinder")
-//            }
-//
-//        }
+        //        VStack{
+        //            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        //
+        //            Spacer()
+        //
+        //            Button {
+        //                //scan garne ho
+        //            } label: {
+        //                Image(systemName: "qrcode.viewfinder")
+        //            }
+        //
+        //        }
         
-        VStack {
-            Image(systemName: "house")
-                .frame(width: 130 , height: 70)
-                .background(
-                    ZStack {
-                        Color.theme.dropShadow
-                        RoundedRectangle(cornerRadius: 50, style: .continuous)
-                            .foregroundColor(.white)
-                            .blur(radius: 4)
-                            .offset(x: -8, y: -8)
-                        
-                        RoundedRectangle(cornerRadius: 50, style: .continuous)
-                            .fill(
-                                LinearGradient(colors: [Color.theme.dropShadow, .white], startPoint: .topLeading, endPoint: .bottomTrailing)
-                            )
-                            .padding(2)
-                            .blur(radius: 2)
-                    }
-                )
-                .clipShape(RoundedRectangle(cornerRadius: 50, style: .continuous))
-                .shadow(color: Color.theme.dropShadow, radius: 20, x: 20, y: 20)
-                .shadow(color: .white, radius: 20, x: -20, y: -20)
+        
+        
+        
+        // neumorphic button
+        //        VStack {
+        //            Image(systemName: "house")
+        //                .frame(width: 130 , height: 70)
+        //                .background(
+        //                    ZStack {
+        //                        Color.theme.dropShadow
+        //                        RoundedRectangle(cornerRadius: 50, style: .continuous)
+        //                            .foregroundColor(.white)
+        //                            .blur(radius: 4)
+        //                            .offset(x: -8, y: -8)
+        //
+        //                        RoundedRectangle(cornerRadius: 50, style: .continuous)
+        //                            .fill(
+        //                                LinearGradient(colors: [Color.theme.dropShadow, .white], startPoint: .topLeading, endPoint: .bottomTrailing)
+        //                            )
+        //                            .padding(2)
+        //                            .blur(radius: 2)
+        //                    }
+        //                )
+        //                .clipShape(RoundedRectangle(cornerRadius: 50, style: .continuous))
+        //                .shadow(color: Color.theme.dropShadow, radius: 20, x: 20, y: 20)
+        //                .shadow(color: .white, radius: 20, x: -20, y: -20)
+        //        }
+        //        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        //        .background(Color.theme.backColor)
+        //        .ignoresSafeArea(.all)
+        //
+        
+        
+        
+        NavigationView {
+            NavigationLink(destination: scanQRView(), isActive: self.$isActive) {
+                VStack {
+                    Image(systemName: "qrcode.viewfinder")
+                        .frame(width: 130 , height: 70)
+                        .background(
+                            ZStack {
+                                Color.theme.dropShadow
+                                RoundedRectangle(cornerRadius: 50, style: .continuous)
+                                    .foregroundColor(.white)
+                                    .blur(radius: 4)
+                                    .offset(x: -8, y: -8)
+                                
+                                RoundedRectangle(cornerRadius: 50, style: .continuous)
+                                    .fill(
+                                        LinearGradient(colors: [Color.theme.dropShadow, .white], startPoint: .topLeading, endPoint: .bottomTrailing)
+                                    )
+                                    .padding(2)
+                                    .blur(radius: 2)
+                            }
+                        )
+                        .clipShape(RoundedRectangle(cornerRadius: 50, style: .continuous))
+                        .shadow(color: Color.theme.dropShadow, radius: 20, x: 20, y: 20)
+                        .shadow(color: .white, radius: 20, x: -20, y: -20)
+                }
+            }
+            .buttonStyle(PlainButtonStyle())
+            
+            
+            
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(Color.theme.backColor)
-        .ignoresSafeArea(.all)
+        
+        
+        
+        
+        
         
     }
 }
